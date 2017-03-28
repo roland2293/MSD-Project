fd2c5b44f13f65967ab154062f640124bc66e018
Paurav Patel <paurav@Pauravs-MacBook-Pro.local>
2017-03-28 18:08:32 -0400

    Added AWS RDS connection ...
    JIRA: TEAM15-19

--- DBLP/src/main/java/Team15/DBLP/db/DBConnection.java
+++ DBLP/src/main/java/Team15/DBLP/db/DBConnection.java
@@ -4,11 +4,14 @@ import java.sql.*;
 
 public class DBConnection {
 	// Change the parameters accordingly.
-	//private static String dbUrl = "jdbc:mysql://127.0.0.1:3306/dblp?useUnicode=true&characterEncoding=utf-8";
-	private static String dbUrl = "jdbc:mysql://127.0.0.1:3306/dblp";
+//	private static String dbUrl = "jdbc:mysql://127.0.0.1:3306/dblp?useUnicode=true&characterEncoding=utf-8";
+	private static String dbUrl = "jdbc:mysql://msd-team15.clseyn6egrcm.us-east-1.rds.amazonaws.com:3306/dblp";
 
-	private static String user = "root";
-	private static String password = "";
+	private static String user = "team15user";
+	private static String password = "team15password";
+	
+//	private static String user = "root";
+//	private static String password = "";
 
 	public static Connection getConn() {
 		try {

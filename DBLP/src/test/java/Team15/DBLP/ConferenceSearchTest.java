6e04cc55054292acecf18eb03c6a2a51650dd5d0
Paurav Patel <paurav@Pauravs-MacBook-Pro.local>
2017-03-29 18:35:06 -0400

    corected the JFrame unit tests ...
    JIRA: TEAM15-26

--- DBLP/src/test/java/Team15/DBLP/ConferenceSearchTest.java
+++ DBLP/src/test/java/Team15/DBLP/ConferenceSearchTest.java
@@ -5,22 +5,34 @@ import static org.junit.Assert.*;
 import javax.swing.JCheckBox;
 import javax.swing.JTextField;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import Team15.DBLP.ui.ConferenceSearch;
 import Team15.DBLP.QueryEngine.SearchParameters;
 
 public class ConferenceSearchTest {
-	
-	static ConferenceSearch  conferenceSearch = new ConferenceSearch();
-	JTextField yearTextField = conferenceSearch.getYearTextField();
-	JTextField keywordTextField = conferenceSearch.getKeywordTextField();
-	JTextField conferenceTextField = conferenceSearch.getConferenceTextField();
-	JCheckBox chckbxServedAsGeneral = conferenceSearch.getChckbxServedAsGeneral();
-	JCheckBox chckbxProgramChair = conferenceSearch.getChckbxProgramChair();
-	JCheckBox chckbxConferenceChair = conferenceSearch.getChckbxConferenceChair();
-	JCheckBox chckbxCommitteMember = conferenceSearch.getChckbxCommitteMember();
-	
+
+	static ConferenceSearch conferenceSearch;
+	JTextField yearTextField;
+	JTextField keywordTextField;
+	JTextField conferenceTextField;
+	JCheckBox chckbxServedAsGeneral;
+	JCheckBox chckbxProgramChair;
+	JCheckBox chckbxConferenceChair;
+	JCheckBox chckbxCommitteMember;
+
+	@Before
+	public void startUp() {
+		conferenceSearch = new ConferenceSearch();
+		yearTextField = conferenceSearch.getYearTextField();
+		keywordTextField = conferenceSearch.getKeywordTextField();
+		conferenceTextField = conferenceSearch.getConferenceTextField();
+		chckbxServedAsGeneral = conferenceSearch.getChckbxServedAsGeneral();
+		chckbxProgramChair = conferenceSearch.getChckbxProgramChair();
+		chckbxConferenceChair = conferenceSearch.getChckbxConferenceChair();
+		chckbxCommitteMember = conferenceSearch.getChckbxCommitteMember();
+	}
 
 	@Test
 	public void testNotNull() {
@@ -32,9 +44,9 @@ public class ConferenceSearchTest {
 		assertNotNull(chckbxConferenceChair);
 		assertNotNull(chckbxCommitteMember);
 	}
-	
+
 	@Test
-	public void testSearchParameters(){
+	public void testSearchParameters() {
 		yearTextField.setText("2006");
 		keywordTextField.setText("Next-Generation Networks, Service-Centric");
 		conferenceTextField.setText("ICCCN");
@@ -42,13 +54,16 @@ public class ConferenceSearchTest {
 		chckbxProgramChair.setSelected(true);
 		chckbxConferenceChair.setSelected(true);
 		chckbxCommitteMember.setSelected(true);
-		
-		SearchParameters searchParameters = conferenceSearch.generateSearchParameters();
-		
+
+		SearchParameters searchParameters = conferenceSearch
+				.generateSearchParameters();
+
 		assertEquals("conference", searchParameters.getSearchType());
 		assertEquals(2006, searchParameters.getYearOfPublication());
-		assertEquals("[Next-Generation Networks, Service-Centric]", searchParameters.getKeywords().toString());
-		assertEquals("[ICCCN]", searchParameters.getConferenceNames().toString());
+		assertEquals("[Next-Generation Networks, Service-Centric]",
+				searchParameters.getKeywords().toString());
+		assertEquals("[ICCCN]", searchParameters.getConferenceNames()
+				.toString());
 		assertEquals(true, searchParameters.isGeneralChair());
 		assertEquals(true, searchParameters.isProgramChair());
 		assertEquals(true, searchParameters.isConferenceChair());
